27345853d2b40c579332ee02781bb817d11b3c03
Thawkster <tristan@nithon.com>
2019-10-10 03:21:01 -0700

    [ADD] did_survive_infection

--- person.py
+++ person.py
@@ -27,7 +27,15 @@ class Person(object):
         '''
         # Only called if infection attribute is not None.
         # TODO:  Finish this method. Should return a Boolean
-        pass
+        chance_of_survival = random.randint(0, 100)
+        if self.infection == None:
+            if chance_of_survival < self.infection.mortality_rate: # compare mortality rates
+                self.is_alive = False
+                return False
+            else:
+                self.is_vaccinated = True
+                self.infection = None
+                return True
 
 
     def is_alive():
@@ -38,6 +46,10 @@ class Person(object):
 
     def is_infected():
         pass
+
+    def survived():
+        if infected(self.person) and is_alive(self.person) == True:
+            return True
 
 ''' These are simple tests to ensure that you are instantiating your Person class correctly. '''
 def test_vacc_person_instantiation():
